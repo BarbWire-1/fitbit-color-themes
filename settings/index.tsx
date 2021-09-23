ae0e3d3d032c95a59f29488b321fa67b724b9a8c
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2021-09-23 14:44:35 +0200

    try color Themes in react

--- settings/index.tsx
+++ settings/index.tsx
@@ -30,7 +30,11 @@
 //   
 //   registerSettingsPage(HelloWorld);
 
-
+let themes = [
+    ['blue', 'red', 'yellow'],
+    ['grey', 'white', 'orange']
+];
+    
 //EXAMPLE
   function Colors(props) {
     return (
@@ -39,9 +43,10 @@
           title={<Text bold align="center">Color Settings</Text>}>
           <ColorSelect
             settingsKey="myColor"
+            
             colors={[
-              {color: 'tomato'},
-              {color: 'sandybrown'},
+              {color: 'tomato', value: themes[0][0]},
+              {color: 'sandybrown', value: themes[1][0]},
               {color: 'gold'},
               {color: 'aquamarine'},
               {color: 'deepskyblue'},
@@ -54,3 +59,48 @@
   }
   
   registerSettingsPage(Colors);
+  
+ 
+  function ListItem(props) {
+    // Correct! There is no need to specify the key here:
+    return <li>{props.value}</li>;
+  }
+  
+  
+  
+ //TODO check this
+  
+//   function NumberList(props) {
+//     const numbers = props.numbers;
+//     const listItems = numbers.map((number) =>
+//       // Correct! Key should be specified inside the array.
+//       <ListItem key={number.toString()} value={number} />
+//     );
+//     return (
+//       <ul>
+//         {listItems}
+//       </ul>
+//     );
+//   }
+//   
+//   const numbers = [1, 2, 3, 4, 5];
+//   console.log(JSON.stringify(<NumberList numbers={numbers}/>),
+//     );
+
+function ColorList(props) {
+    const myColors = props.cols;
+    const listItems = cols.map((col) =>
+      // Correct! Key should be specified inside the array.
+      <ListItem key={col} value={col} />
+    );
+    return (
+      <ul>
+        {listItems}
+      </ul>
+    );
+  }
+  
+  const cols = ['red','blue','green'];
+  console.log(JSON.stringify(<ColorList cols={cols}/>),
+    );
+   
