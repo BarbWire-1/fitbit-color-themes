9525fb26aacfbade92d0cee2e4520122acc5fd87
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2021-09-23 19:10:43 +0200

    strange line

--- app/index.ts
+++ app/index.ts
@@ -18,20 +18,24 @@ messaging.peerSocket.addEventListener("message", (evt) =>
 {   
     //evt.data.key here colorThemes (colors []), evt.data.value here the index of chosen theme
     // in settings/index.tsx as item of colors like {color: 'colorShows', value: 0},
-    if (evt && evt.data && evt.data.key === "colorTheme") console.log("Theme colors: " + themes[evt.data.value]);
+    if (evt && evt.data && evt.data.key === "colorTheme") 
+    
+    //TODO WHY???? does it only work with this line?
+    //Where have I messed that up?
+    console.log("Theme colors: " + themes[evt.data.value]);
+    {
+        //console.log("Colors of theme: " + themes[evt.data.value]);
+        //Iterate through number of colors per theme 
+        //and assign color [i] per className where className = color+i
+        for(let i = 0; i<themes[evt.data.value].length; i++)
         {
-            console.log("Colors of theme: " + themes[evt.data.value]);
-            //Iterate through number of colors per theme 
-            //and assign color [i] per className where className = color+i
-            for(let i = 0; i<themes[evt.data.value].length; i++)
+            (document.getElementsByClassName("color" + i) as GraphicsElement[]).forEach((el) =>
             {
-                (document.getElementsByClassName("color" + i) as GraphicsElement[]).forEach((el) =>
-                {
                 console.log("color " + i + ": " + themes[evt.data.value][i]);
                 el.style.fill = themes[evt.data.value][i];
-                })
-            }
+            })
         }
-    });
+    }
+});
 
 
--- settings/index.tsx
+++ settings/index.tsx
@@ -1,38 +1,4 @@
-// registerSettingsPage(({ settings: { myProp } }) => (
-//     <Page>
-//         <Section title="Settings">{myProp && <Text>{myProp}</Text>}</Section>
-//     </Page>
-// ));
-// function HelloWorld(props) {
-//     return (
-//       <Page>
-//         <Section
-//           title={<Text bold align="center">Demo Settings</Text>}>
-//           <Toggle
-//             settingsKey="toggle"
-//             label="Toggle Switch"
-//           />
-//           <ColorSelect
-//             settingsKey="color"
-//             colors={[
-//               {color: 'tomato'},
-//               {color: 'sandybrown'},
-//               {color: 'gold'},
-//               {color: 'aquamarine'},
-//               {color: 'deepskyblue'},
-//               {color: 'plum'}
-//             ]}
-//           />
-//         </Section>
-//       </Page>
-//     );
-//   }
-//   
-//   registerSettingsPage(HelloWorld);
 
-
-    
-//EXAMPLE
   function Colors(props) {
     return (
       <Page>
@@ -57,47 +23,3 @@
   
   registerSettingsPage(Colors);
   
- //TODO check this
-//   function ListItem(props) {
-//     // Correct! There is no need to specify the key here:
-//     return <li>{props.value}</li>;
-//   }
-//   
-//   
-//   
-//  
-//   
-//   function NumberList(props) {
-//     const numbers = props.numbers;
-//     const listItems = numbers.map((number) =>
-//       // Correct! Key should be specified inside the array.
-//       <ListItem key={number.toString()} value={number} />
-//     );
-//     return (
-//       <ul>
-//         {listItems}
-//       </ul>
-//     );
-//   }
-//   
-//   const numbers = [1, 2, 3, 4, 5];
-//   console.log(JSON.stringify(<NumberList numbers={numbers}/>),
-//     );
-
-// function ColorList(props) {
-//     const myColors = props.cols;
-//     const listItems = cols.map((col) =>
-//       // Correct! Key should be specified inside the array.
-//       <ListItem key={col} value={col} />
-//     );
-//     return (
-//       <ul>
-//         {listItems}
-//       </ul>
-//     );
-//   }
-//   
-//   const cols = ['red','blue','green'];
-//   console.log(JSON.stringify(<ColorList cols={cols}/>),
-//     );
-   
