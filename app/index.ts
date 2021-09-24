56916ad69b2b7494e1eee31f1048fdfe027ac4ec
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2021-09-24 14:01:22 +0200

    common/color

--- app/index.ts
+++ app/index.ts
@@ -1,53 +1,9 @@
 import * as messaging from "messaging";
 import document  from "document";
 import { preferences} from "../common/shared_preferences";
+import {applyColors, prefColor} from "../common/colorThemes";
 
 
-//COLOR-THEMES
-//colors - get theme t per data.value
-const themes: string[][] = 
-[
-    ['tomato', 'blue', 'yellow'],
-    ['sandybrown', 'white', 'grey'],
-    ['gold', 'limegreen', 'black'],
-    ['aquamarine', 'turquoise', 'yellow'],
-    ['deepskyblue', 'blue', 'red'],
-    ['plum', 'magenta', 'white'],
-];
-
-////I'd like to send these values to <colors> in settings
-// let setColors: string[] = [];
-// for(let i: number = 0; i<themes.length; i++)
-// {
-//     setColors[i]= (themes[i][0]);  
-// };
-// console.log("setColors: "+ setColors);
-//setColors = preferences.setColors;
-
-let prefColor: number = preferences.prefColor ?? 0;
-
-//apply theme-colors per class
-function applyColors(theme: number){
-    for(let c:number = 0; c<themes[theme].length; c++)
-        {
-            (document.getElementsByClassName("color" + c) as GraphicsElement[]).forEach((el) =>
-            {
-                el.style.fill = themes[theme][c];   
-            });
-        };
-        prefColor = theme;
-};
 applyColors(prefColor);//initial themes[0]
 
-//get themes[t] on evt then call applyColors
-messaging.peerSocket.addEventListener("message", (evt) => 
-{   
-    if (evt?.data?.value && evt.data.key === "ColorTheme") 
-    {   
-        let t: number = evt.data.value;
-        applyColors(t);
-        //save t to "settingsTestColor.cbor"
-        preferences.prefColor = t;
-    };
-});
 
---
+++ common/colorThemes.ts
@@ -0,0 +1,54 @@
+import * as messaging from "messaging";
+import document  from "document";
+import { preferences} from "./shared_preferences";
+
+
+//COLOR-THEMES
+//colors - get theme t per data.value
+const themes: string[][] = 
+[
+    ['tomato', 'blue', 'yellow'],
+    ['sandybrown', 'white', 'grey'],
+    ['gold', 'limegreen', 'black'],
+    ['aquamarine', 'turquoise', 'yellow'],
+    ['deepskyblue', 'blue', 'red'],
+    ['plum', 'magenta', 'white'],
+];
+
+////I'd like to send these values to <colors> in settings
+// let setColors: string[] = [];
+// for(let i: number = 0; i<themes.length; i++)
+// {
+//     setColors[i]= (themes[i][0]);  
+// };
+// console.log("setColors: "+ setColors);
+//setColors = preferences.setColors;
+
+let prefColor: number = preferences.prefColor ?? 0;
+
+//apply theme-colors per class
+function applyColors(theme: number){
+    for(let c:number = 0; c<themes[theme].length; c++)
+        {
+            (document.getElementsByClassName("color" + c) as GraphicsElement[]).forEach((el) =>
+            {
+                el.style.fill = themes[theme][c];   
+            });
+        };
+        prefColor = theme;
+};
+applyColors(prefColor);//initial themes[0]
+
+//get themes[t] on evt then call applyColors
+messaging.peerSocket.addEventListener("message", (evt) => 
+{   
+    if (evt?.data?.value && evt.data.key === "ColorTheme") 
+    {   
+        let t: number = evt.data.value;
+        applyColors(t);
+        //save t to "settingsTestColor.cbor"
+        preferences.prefColor = t;
+    };
+});
+export {applyColors, prefColor};
+
