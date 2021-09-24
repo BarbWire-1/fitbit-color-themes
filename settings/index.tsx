a507154bbec2392993dd64f247bd6f72f13e96ae
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2021-09-24 14:42:28 +0200

    working, but error on load

--- settings/index.tsx
+++ settings/index.tsx
@@ -1,5 +1,5 @@
 import {setColors} from "../common/colorThemes";
-
+let dots = setColors;
 
   function Colors(props) {
     return (
@@ -10,12 +10,12 @@ import {setColors} from "../common/colorThemes";
             settingsKey="ColorTheme"
             
             colors={[
-              {color: setColors[0], value: 0},
-              {color: setColors[1], value: 1},
-              {color: setColors[2], value: 2},
-              {color: setColors[3], value: 3},
-              {color: setColors[4], value: 4},
-              {color: setColors[5], value: 5}
+              {color: dots[0], value: 0},
+              {color: dots[1], value: 1},
+              {color: dots[2], value: 2},
+              {color: dots[3], value: 3},
+              {color: dots[4], value: 4},
+              {color: dots[5], value: 5}
             ]}
           />
         </Section>
